324dfb794c962b6b99d4dc7bbc911b32962708d5
Nicolas-GR <nguerreror@unal.edu.co>
2016-09-19 22:49:47 -0500

    Declaracion de clases

---
+++ LibroCalificaciones/LibroCalificaciones.java
@@ -0,0 +1,7 @@
+public class LibroCalificaciones {
+
+    public void mostrarMensaje(){
+        System.out.println("Bienvenido al Libro de Calificaciones");
+    }
+
+}
---
+++ LibroCalificaciones/PruebaLibroCalificaciones.java
@@ -0,0 +1,10 @@
+public class PruebaLibroCalificaciones{
+
+    public static void main (String[] args){
+        
+        LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones();
+        
+        miLibroCalificaciones.mostrarMensaje();
+    }
+
+}
